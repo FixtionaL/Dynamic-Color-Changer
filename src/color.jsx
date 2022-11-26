4ca643c711a1e32ef909e5a372f456279bdf9136
Harsh Kumar <harshng7@gmail.com>
2022-11-26 16:14:57 +0530

    grammer

--- src/color.jsx
+++ src/color.jsx
@@ -6,7 +6,7 @@ function Dropdown(colors) {
     return (
         <div className="dropdown">
             <div id='hehe' className="dropdown-btn" onClick={() => setIsOpen(!isOpen)}>
-                <p id="label">Choose Your</p>
+                <p id="label">Choose Your Color</p>
                 {isOpen ? (
                     <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6.1018 16.9814C5.02785 16.9814 4.45387 15.7165 5.16108 14.9083L10.6829 8.59762C11.3801 7.80079 12.6197 7.80079 13.3169 8.59762L18.8388 14.9083C19.5459 15.7165 18.972 16.9814 17.898 16.9814H6.1018Z" fill="#212121" />
