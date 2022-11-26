ce1add2b1be66db48bc938bef6b7b968126f856b
Harsh Kumar <harshng7@gmail.com>
2022-11-26 15:53:01 +0530

    no final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Dropdown from './color';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    var options = [
+        { 'label': 'Lime', 'value': 'lime' },
+        { 'label': 'Lavender', 'value': 'lavender' },
+        { 'label': 'Crimson', 'value': 'crimson' },
+        { 'label': 'Darkblue', 'value': 'darkblue' },
+        { 'label': 'Teal', 'value': 'teal' },
+        { 'label': 'Rebecca Purple', 'value': 'rebeccapurple' },
+        { 'label': 'Ghost White', 'value': 'ghostwhite' },
+        { 'label': 'Aqua Marine', 'value': 'aquamarine' },
+        { 'label': 'Alice Blue', 'value': 'aliceblue' }
+    ];
+    return (
+        <div className="App">
+            <h1 className='mainH'>Dynamic Color Changer</h1>
+            <Dropdown options={options} />
+            <div className="box">
+                <div id='noob' className='area'>Magic Box</div> 
+            </div>
+        </div>
+    );
 }
 
 export default App;
